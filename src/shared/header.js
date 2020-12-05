26b6c386e15a287d575f6006a5f3c60b5666ddc4
unknown <alaeddin.tabai@esprit.tn>
2020-12-05 14:17:02 +0100

    header-section

--- src/shared/header.js
+++ src/shared/header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../style/header/_header.scss'
 
 export function Header(){
 
     return(
-        
-        <h1>Header Section</h1>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">MyClone</a>
+            <form class="form-inline">
+                <input className="form-control mr-sm-2 mySearchInput" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-primery my-2 my-sm-0 mySearchButton" type="submit">Search</button>
+                </form>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav ml-auto">
+                <a className="nav-link" href="#">Login</a>
+                <a className="nav-link" href="#">Register</a>
+                </div>
+            </div>
+        </nav>
     )
 
 }
