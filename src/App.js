bbff10912c726609c0c5cfe695e7f4940d69585e
unknown <alaeddin.tabai@esprit.tn>
2020-12-05 20:45:05 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import logo from './logo.svg';
 import React , {useState , useEffect} from 'react'
 import './App.css';
 import {Header} from './shared/header'
+import {BrowserRouter,Route} from 'react-router-dom';
 import RentalListHooks from './components/rentalComponent/RenderListHooks';
 import RentalDetails from './components/rentalComponent/RentalDetails';
 
 function App() {
-
-    const [isRentalList , setIsRentalList]= useState(true);
-
-    const changeRoot = ()=>{
-
-        { isRentalList ?setIsRentalList(false) :setIsRentalList(true)};
-    }
-
     return (
-        <div className="App">
-            <Header/>
-            <button onClick={()=>changeRoot()}>change</button>
-            <div className='container'>
-                { isRentalList ?<RentalListHooks/> :<RentalDetails/>}  
-            </div> 
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <div className='container'>
+                    <Route exact path="/" component={RentalListHooks} />
+                    <Route exact path="/rentalDetails" component={RentalDetails} /> 
+                </div> 
+            </div>
+        </BrowserRouter>
+
             );
                 }
 
